2039236b01dc0730493c0d36e280cbe7803e91d9
yesuAjith <120998320+yesuAjith@users.noreply.github.com>
2024-01-25 19:00:34 +0530

    Added Jwt configuration (#8)

--- tvminfotech-employee-services-main/src/main/java/com/tvmemp/OpenAPIConfig.java
+++ tvminfotech-employee-services-main/src/main/java/com/tvmemp/OpenAPIConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 
@@ -11,9 +12,20 @@ import io.swagger.v3.oas.annotations.servers.Server;
 
 		servers = { @Server(description = "Developer", url = "http://localhost:8080")
 
-		})
-
-@SecurityScheme(name = "Author", in = SecuritySchemeIn.HEADER, type = SecuritySchemeType.HTTP, bearerFormat = "JWT", description = "Security Desc")
+		},
+		
+security = @SecurityRequirement(
+		name = "authBearer"
+		)
+)
+@SecurityScheme(
+		name = "authBearer",
+		in = SecuritySchemeIn.HEADER,
+		type = SecuritySchemeType.HTTP,
+		bearerFormat ="jwt",
+		scheme = "bearer",
+		description = "Security desc"
+		)
 public class OpenAPIConfig {
 
 }
