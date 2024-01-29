6edb3a710fb1061a9b27e313c6753e66c314dce4
yesuAjith <yesupragasaajith@gmail.com>
2024-01-29 16:32:45 +0530

    Added the logger and comments

--- tvminfotech-employee-services-main/src/main/java/com/tvmemp/config/SecurityConfiguration.java
+++ tvminfotech-employee-services-main/src/main/java/com/tvmemp/config/SecurityConfiguration.java
@@ -33,7 +33,7 @@ public class SecurityConfiguration {
         http.csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/auth/**","/swagger-ui/**", "/v3/api-docs/**","/h2-console/**")
+                .requestMatchers("/auth/**","/swagger-ui/**", "/v3/api-docs/**","/h2-console/**","/favicon.ico")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
